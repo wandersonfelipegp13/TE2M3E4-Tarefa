e33c536d16f68665debc20bfcc0e37b51d22a24e
Wanderson <wandersonfelipeplus13@gmail.com>
2021-07-06 22:26:56 -0300

    Estrutura da tabela legislatura criada.

--- src/main/java/util/dados.sql
+++ src/main/java/util/dados.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS dados;
+
 CREATE DATABASE dados;
 
 USE dados;
@@ -8,4 +10,12 @@ CREATE TABLE legislatura (
 	dataInicio DATE NOT NULL,
 	dataFim DATE NOT NULL,
 	anoEleicao INT NOT NULL
+);
+
+INSERT INTO legislatura VALUES 
+("56", "https://dadosabertos.camara.leg.br/api/v2/legislaturas/%s56", "2019-02-01", "2023-01-31", 2018),
+("55", "https://dadosabertos.camara.leg.br/api/v2/legislaturas/%s55", "2015-02-01", "2019-01-31", 2014);
+
+CREATE TABLE deputado (
+	
 );
